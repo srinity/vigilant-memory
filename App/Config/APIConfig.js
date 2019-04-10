4f366d29200f0813b54a59b650e40530d1ea4c91
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-04-10 03:35:40 +0200

    Modified URLS

--- App/Config/APIConfig.js
+++ App/Config/APIConfig.js
@@ -1,20 +1,20 @@
 import Axios from 'axios';
 
 export const APIURLs = {
-    baseURL: 'http://test.com', 
-    login: '/api/login',
-    register: '/api/register'
+  baseURL: 'http://bd69e752.ngrok.io',
+  login: '/api/login',
+  register: '/api/users/signin'
 };
 
 export const APIHeaders = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'appversionheader': '1.0'
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'appversionheader': '1.0'
 };
 
 export const AppAxios = Axios.create({
-    baseURL: APIURLs.baseURL,
-    headers: {
-        ...APIHeaders
-    }
+  baseURL: APIURLs.baseURL,
+  headers: {
+    ...APIHeaders
+  }
 });
