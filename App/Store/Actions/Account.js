d96a42c2e9cd368df5d8464fefc15b356d29d56c
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-04-10 03:31:23 +0200

    Modified the register action

--- App/Store/Actions/Account.js
+++ App/Store/Actions/Account.js
@@ -1,92 +1,95 @@
 import {
-    LOGIN_REQUEST_STARTED,
-    LOGIN_REQUEST_SUCCESS,
-    LOGIN_REQUEST_FAILED,
-    REGISTER_REQUEST_STARTED,
-    REGISTER_REQUEST_SUCCESS,
-    REGISTER_REQUEST_FAILED,
-    LOGOUT_REQUEST_SUCCESS,
-    LOGOUT_REQUEST_FAILED,
-    LOGOUT_REQUEST_STARTED
+  LOGIN_REQUEST_STARTED,
+  LOGIN_REQUEST_SUCCESS,
+  LOGIN_REQUEST_FAILED,
+  REGISTER_REQUEST_STARTED,
+  REGISTER_REQUEST_SUCCESS,
+  REGISTER_REQUEST_FAILED,
+  LOGOUT_REQUEST_SUCCESS,
+  LOGOUT_REQUEST_FAILED,
+  LOGOUT_REQUEST_STARTED
 } from './ActionTypes';
 
 import { APIURLs, AppAxios } from './../../Config/APIConfig';
 
 export const login = (email, password) => {
-    return async (dispatch) => {
-        try {
-            dispatch(loginRequestStarted());
-            const response = await AppAxios.post(APIURLs.login, { email, password });
-            console.tron.log(response.data);
-            dispatch(loginRequestSuccess(response.data));
-        } catch (error) {
-            console.tron.error(error);
-            console.tron.error(error.response);
-            dispatch(loginRequestFailed(error.response));
-        }
-    };
+  return async (dispatch) => {
+    try {
+      dispatch(loginRequestStarted());
+      const response = await AppAxios.post(APIURLs.login, { email, password });
+      console.tron.log(response.data);
+      dispatch(loginRequestSuccess(response.data));
+    } catch (error) {
+      console.tron.error(error);
+      console.tron.error(error.response);
+      dispatch(loginRequestFailed(error.response));
+    }
+  };
 };
 
-export const register = (name, email, password, address, gender) => {
-    return async (dispatch) => {
-        try {
-            dispatch(registerRequestStarted());
+export const register = (firstName, lastName, email, password, phone) => {
+  return async (dispatch) => {
+    try {
+      dispatch(registerRequestStarted());
 
-            const response = await AppAxios.post(APIURLs.register, {
-                name,
-                email,
-                password,
-                address,
-                gender
-            });
-            console.tron.log(response);
-            dispatch(registerRequestSuccess(response.data));
-        } catch (error) {
-            console.tron.error(error);
-            dispatch(registerRequestFailed(error.response));
-        }
-    };
+      const response = await AppAxios.post(APIURLs.register, {
+        fullName: {
+          firstName,
+          lastName
+        },
+        email,
+        password,
+        phone,
+        userRole: 'user'
+      });
+      console.tron.log(response);
+      dispatch(registerRequestSuccess(response.data));
+    } catch (error) {
+      console.tron.error(error);
+      dispatch(registerRequestFailed(error.response));
+    }
+  };
 };
 
 export const logout = () => {
-    return dispatch => {
-        // dispatch(logoutRequestStarted());
-        dispatch(logoutRequestSuccess());
-    };
+  return dispatch => {
+    // dispatch(logoutRequestStarted());
+    dispatch(logoutRequestSuccess());
+  };
 };
 
 function loginRequestStarted() {
-    return { type: LOGIN_REQUEST_STARTED };
+  return { type: LOGIN_REQUEST_STARTED };
 }
 
 function loginRequestSuccess(user) {
-    return { type: LOGIN_REQUEST_SUCCESS, user };
+  return { type: LOGIN_REQUEST_SUCCESS, user };
 }
 
 function loginRequestFailed(error) {
-    return { type: LOGIN_REQUEST_FAILED, error };
+  return { type: LOGIN_REQUEST_FAILED, error };
 }
 
 function registerRequestStarted() {
-    return { type: REGISTER_REQUEST_STARTED };
+  return { type: REGISTER_REQUEST_STARTED };
 }
 
 function registerRequestSuccess(user) {
-    return { type: REGISTER_REQUEST_SUCCESS, user };
+  return { type: REGISTER_REQUEST_SUCCESS, user };
 }
 
 function registerRequestFailed(error) {
-    return { type: REGISTER_REQUEST_FAILED, error };
+  return { type: REGISTER_REQUEST_FAILED, error };
 }
 
 function logoutRequestStarted() {
-    return { type: LOGOUT_REQUEST_STARTED };
+  return { type: LOGOUT_REQUEST_STARTED };
 }
 
 function logoutRequestSuccess() {
-    return { type: LOGOUT_REQUEST_SUCCESS, user: null };
+  return { type: LOGOUT_REQUEST_SUCCESS, user: null };
 }
 
 function logoutRequestFailed(error) {
-    return { type: LOGOUT_REQUEST_FAILED, error };
+  return { type: LOGOUT_REQUEST_FAILED, error };
 }
