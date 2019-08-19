6dbc7f68ccc104340bc5b24a5182aeb2ec0948d4
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-08-19 06:03:38 +0200

    [feat] modified home UI and integreated with API

--- App/Screens/Home/Home.Styles.js
+++ App/Screens/Home/Home.Styles.js
@@ -1,4 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+
+import { isIPhoneDeviceWithNotch } from './../../Utils/Platform';
+
+import { Colors } from './../../Theme';
 
 export default StyleSheet.create({
   containerStyle: {
@@ -6,5 +10,51 @@ export default StyleSheet.create({
   },
   flatListStyle: {
     flex: 1
+  },
+  headerContainerStyle: {
+    backgroundColor: Colors.brandColorHexCode,
+    paddingHorizontal: 10,
+    ...Platform.select({
+      android: {
+        paddingVertical: 10
+      },
+      ios: {
+        paddingTop: isIPhoneDeviceWithNotch() ? 40 : 0,
+        paddingBottom: isIPhoneDeviceWithNotch() ? 10 : 0,
+        paddingVertical: isIPhoneDeviceWithNotch() ? 0 : 10
+      }
+    })
+  },
+  searchBoxContainerStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: Colors.whiteColorHexCode,
+    borderRadius: 25
+  },
+  searchTextStyle: {
+    color: Colors.blackColorHexCode,
+    fontSize: 16,
+    marginHorizontal: 10,
+    flex: 1
+  },
+  searchSelectionContainerStyle: {
+    flex: 2,
+    justifyContent: 'center',
+    width: 250,
+    alignSelf: 'center'
+  },
+  searchButtonStyle: {
+    flex: 0,
+    marginVertical: 15,
+    backgroundColor: Colors.brandColorHexCode
+  },
+  disabledSearchButtonStyle: {
+    backgroundColor: Colors.getBrandColorRGBAValue(0.5)
+  },
+  backContainerStyle: {
+    marginLeft: Platform.OS === 'ios' ? 10 : 20,
+    marginTop: isIPhoneDeviceWithNotch() ? 50 : 20
   }
 });
--- App/Screens/Home/Home.js
+++ App/Screens/Home/Home.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { ImageBackground, SafeAreaView, FlatList } from 'react-native';
+import { ImageBackground, SafeAreaView, FlatList, View, Text, TouchableWithoutFeedback } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Dropdown } from 'react-native-material-dropdown';
 
-import { ShopCard } from './../../Components';
+import { ShopCard, Icon, IconTypes, Logo, Button } from './../../Components';
 
-import { Images } from './../../Theme';
+import { Images, Colors } from './../../Theme';
 
 import styles from './Home.Styles';
 
 class Home extends Component {
+  static getDerivedStateFromProps(props, state) {
+    if (state.isLoading && !props.isLoading && props.shops !== null) {
+      return { isLoading: props.isLoading, showSearch: false };
+    } else if (state.isLoading !== props.isLoading) {
+      return { isLoading: props.isLoading };
+    }
+
+    return null;
+  }
+
+  state = {
+    isLoading: false,
+    showSearch: true,
+    areas: null,
+    districts: null,
+    selectedCity: undefined,
+    selectedArea: undefined,
+    selectedDistrict: undefined
+  }
+
   componentDidMount() {
-    this.props.getShops();
+    this.props.getSearchAreas();
   }
 
   onShopPress = (shopInfo) => {
@@ -18,13 +39,48 @@ class Home extends Component {
     Actions.shop({ title: shopInfo.shopName, ...shopInfo });
   }
 
+  onCityValueChange = (selectedCity) => {
+    this.setState({
+      areas: selectedCity.areas,
+      selectedCity: selectedCity.city,
+      selectedArea: '',
+      selectedDistrict: ''
+    });
+  }
+
+  onAreaValueChange = (selectedArea) => {
+    this.setState({
+      districts: selectedArea.districts,
+      selectedArea: selectedArea.area,
+      selectedDistrict: ''
+    });
+  }
+
+  onDistrictValueChange = (selectedDistrict) => {
+    this.setState({ selectedDistrict });
+  }
+
+  onSearchToolBarPress = () => {
+    this.setState({ showSearch: true });
+  }
+
+  onBackIconPress = () => {
+    this.setState({ showSearch: false });
+  }
+
+  onSearchPress = () => {
+    const { selectedArea, selectedCity, selectedDistrict } = this.state;
+
+    this.props.getShops(selectedCity, selectedArea, selectedDistrict);    
+  }
+
   renderItem = ({ item }) => {
     return (
       <ShopCard
-        key={item.shopName}
+        key={item._id}
         name={item.shopName}
-        address={item.address}
-        image={item.shopImage}
+        address={`${item.address.district}, ${item.address.area}, ${item.address.city}`}
+        image={`https://s3-us-west-2.amazonaws.com/elcartona/${item.shopImage}`}
         height={this.props.height / 2.5}
         onPress={() => this.onShopPress(item)}
       />
@@ -32,18 +88,96 @@ class Home extends Component {
   }
 
   render() {
+    const { showSearch, selectedArea, selectedCity, selectedDistrict } = this.state;
+    const { isLoading } = this.props;
     console.tron.error(this.props);
+    console.tron.error(this.state);
 
     return (
-      <ImageBackground source={Images.cover2} style={styles.containerStyle}>
-        <SafeAreaView style={styles.containerStyle}>
-          <FlatList
-            data={this.props.shops}
-            renderItem={this.renderItem}
-            style={{ flex: 1 }}
-          />
-        </SafeAreaView>
-      </ImageBackground>
+      showSearch
+      ?
+      (
+        <View style={styles.containerStyle}>
+          {
+            this.props.shops
+            ?
+            <TouchableWithoutFeedback onPress={this.onBackIconPress}>
+              <View style={styles.backContainerStyle}>
+                <Icon type={IconTypes.ant} name='arrowleft' size={20} />
+              </View>
+            </TouchableWithoutFeedback>
+            :
+            null
+          }
+          <Logo name='logo' />
+
+          <View style={styles.searchSelectionContainerStyle}>
+            <Dropdown
+              label='City'
+              value={selectedCity}
+              data={this.props.cities || []}
+              onChangeText={this.onCityValueChange}
+              valueExtractor={item => item}
+              labelExtractor={item => item.city}
+              baseColor={Colors.brandColorHexCode}
+              dropdownPosition={2}
+            />
+
+            <Dropdown
+              label='Area'
+              value={selectedArea}
+              data={this.state.areas || []}
+              onChangeText={this.onAreaValueChange}
+              valueExtractor={item => item}
+              labelExtractor={item => item.area}
+              disabled={!this.state.areas}
+              baseColor={Colors.brandColorHexCode}
+              dropdownPosition={2}
+            />
+
+            <Dropdown
+              label='District'
+              value={selectedDistrict}
+              data={this.state.districts || []}
+              onChangeText={this.onDistrictValueChange}
+              disabled={!this.state.districts}
+              valueExtractor={item => item}
+              baseColor={Colors.brandColorHexCode}
+              dropdownPosition={2}
+            />
+
+            <Button
+              title='Search'
+              disabled={!selectedCity || !selectedArea || !selectedDistrict}
+              isLoading={isLoading}
+              style={styles.searchButtonStyle}
+              disabledStyle={styles.disabledSearchButtonStyle}
+              onPress={this.onSearchPress}
+            />
+          </View>
+        </View>
+      )
+      :
+      (
+        <ImageBackground source={Images.cover2} style={styles.containerStyle}>
+          <View style={styles.headerContainerStyle}>
+            <TouchableWithoutFeedback onPress={this.onSearchToolBarPress}>
+              <View style={styles.searchBoxContainerStyle}>
+                <Icon type={IconTypes.oct} name='search' size={15} />
+                <Text style={styles.searchTextStyle} numberOfLines={1}>{`${selectedDistrict || 'District'}, ${selectedArea || 'Area'}, ${selectedCity || 'City'}`}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+
+          <SafeAreaView style={styles.containerStyle}>
+            <FlatList
+              data={this.props.shops}
+              renderItem={this.renderItem}
+              style={{ flex: 1 }}
+            />
+          </SafeAreaView>
+        </ImageBackground>
+      )
     );
   }
 }
