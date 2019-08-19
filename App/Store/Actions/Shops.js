232953c595347746e48b878690ba60ea0443fd0e
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-08-19 06:03:12 +0200

    [feat] Added searchAreas and getShops api

--- App/Store/Actions/Shops.js
+++ App/Store/Actions/Shops.js
@@ -3,52 +3,51 @@ import { APIURLs, AppAxios } from './../../Config/APIConfig';
 import {
     GET_SHOPS_REQUEST_STARTED,
     GET_SHOPS_REQUEST_FAILED,
-    GET_SHOPS_REQUEST_SUCCESS
+    GET_SHOPS_REQUEST_SUCCESS,
+    GET_SEARCH_AREAS_SUCCESS,
+    GET_SEARCH_AREAS_FAILED
 } from './ActionTypes';
 
-import { Images } from './../../Theme';
-
-const dummyShops = [
-    {
-      shopName: 'Some Shop Name',
-      shopImage: Images.cover,
-      address: 'Some Shop Adress, more address, address!!'
-    },
-    {
-      shopName: 'Another Shop!',
-      shopImage: 'https://picsum.photos/200/300/?random',
-      address: 'This is the other shop Address!ß'
-    },
-    {
-      shopName: 'Some Other Shop Name',
-      shopImage: 'https://picsum.photos/200/300/?random',
-      address: 'Some Shop Adress, more address, address!!'
-    },
-    {
-      shopName: 'Fat7 Allah',
-      shopImage: 'https://picsum.photos/200/300/?random',
-      address: 'Some Shop Adress, more address, address!!'
-    },
-    {
-      shopName: 'Mdenah',
-      shopImage: 'https://picsum.photos/200/300/?random',
-      address: 'Some Shop Adress, more address, address!!'
-    },
-    {
-      shopName: 'Some Shop Name',
-      shopImage: 'https://picsum.photos/600/600/?random',
-      address: 'Some Shop Adress, more address, address!!'
+export const getSearchAreas = () => {
+  return async dispatch => {
+    try {
+      console.tron.error('hereeeeeee')
+      const response = await AppAxios.get(APIURLs.searchAreas);
+      console.tron.warn(response)
+      dispatch(getSearchAreasSuccess(response.data.cities));
+    } catch (error) {
+      console.tron.log(error)
+      dispatch(getSearchAreasFailed(error));
     }
-];
+  };
+};
 
-export const getShops = () => {
+export const getShops = (city, area, district) => {
     return async (dispatch) => {
         dispatch(getShopsStarted());
-        setTimeout(() => {
-            dispatch(getShopsSuccess(dummyShops));
-        }, 500);
+        try {
+          const response = await AppAxios.get(APIURLs.getShops, {
+            params: {
+              city,
+              area,
+              district
+            }
+          });
+
+          dispatch(getShopsSuccess(response.data.shops));
+        } catch (error) {
+          dispatch(getShopsFailed(error));
+        }
     };
 };
+
+function getSearchAreasSuccess(cities) {
+  return { type: GET_SEARCH_AREAS_SUCCESS, cities };
+}
+
+function getSearchAreasFailed(error) {
+  return { type: GET_SEARCH_AREAS_FAILED, error };
+}
 
 function getShopsStarted() {
     return { type: GET_SHOPS_REQUEST_STARTED };
