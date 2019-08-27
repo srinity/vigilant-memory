2c8004af8dbebfbc48ab27a0433f3ac44065219f
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-08-27 05:54:21 +0200

    [feat] Added new API end points

--- App/Config/APIConfig.js
+++ App/Config/APIConfig.js
@@ -1,11 +1,13 @@
 import Axios from 'axios';
 
 export const APIURLs = {
-  baseURL: 'https://a6dfc912.ngrok.io',
+  baseURL: 'https://1d25978a.ngrok.io',
   login: '/api/login',
   register: '/api/users/signin',
   searchAreas: '/api/admin/addresses/city',
-  getShops: '/api/client/shop/getShopsAtSpecificArea'
+  getShops: '/api/client/shop/getShopsAtSpecificArea',
+  getProductsOfShopGroupedByCategory: '/api/client/shop/getProductsOfShopGroupedByCategory',
+  getProductsOfShopBasedOnFilter: '/api/client/shop/getProductsOfShopBasedOnFilter'
 };
 
 export const ImageHostUrl = 'https://s3-us-west-2.amazonaws.com/elcartona/';
