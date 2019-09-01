dc8b6b1d16913ddd354731e7eb3e9c940da055f8
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-09-01 13:38:26 +0200

    [feat] Added Cart Api and handled offline and online mode

--- App/Store/Actions/Cart.js
+++ App/Store/Actions/Cart.js
@@ -5,7 +5,11 @@ import {
     remove as _remove,
     unset as _unset,
     findIndex as _findIndex,
-    find as _find
+    find as _find,
+    isNil as _isNil,
+    map as _map,
+    flatten as _flatten,
+    forEach as _forEach
 } from 'lodash';
 
 import { APIURLs, AppAxios } from './../../Config/APIConfig';
@@ -16,66 +20,171 @@ import {
     REMOVE_ITEM_FROM_CART,
     BUY_SHOP_CART_PRODUCTS_STARTED,
     BUY_SHOP_CART_PRODUCTS_FAILED,
-    BUY_SHOP_CART_PRODUCTS_SUCCESS
+    BUY_SHOP_CART_PRODUCTS_SUCCESS,
+    CART_ITEMS_UPDATE_STARTED,
+    CART_ITEMS_UPDATE_FAILED,
+    FAILED_TO_UPLOAD_USER_CART_UPON_LOGIN,
+    GET_USER_CART_STARTED,
+    GET_USER_CART_SUCCESS,
+    GET_USER_CART_FAILED
 } from './ActionTypes';
 
-export function addToCart(shopId, shopName, product, cart) {
-    let shopCartItems = { shopId, shopName, products: [] };
+const CART_ITEMS_API_ACTIONS = {
+    add: 'ADD_ITEM',
+    remove: 'DELETE_ITEM',
+    change: 'CHANGE_QUANTITY'
+};
 
-    if (_has(cart, shopId)) {
-        shopCartItems = cart[shopId];
-    }
+export function getUserCart(user) {
+    return async dispatch => {
+        dispatch(getUserCartStarted());
 
-    shopCartItems.products.push(product);
-
-    const newCart = _cloneDeep(cart);
-    newCart[shopId] = shopCartItems;
-
-    console.tron.error(newCart);
-
-    return { type: ADD_ITEM_TO_CART, cart: newCart };
-}
-
-export function changeCartProductQuantity(shopId, shopName, product, cart) {
-    let shopCartItems = { shopId, shopName, products: [] };
-
-    if (_has(cart, shopId)) {
-        shopCartItems = cart[shopId];
-    }
-
-    const productIndex = _findIndex(shopCartItems.products, item => item._id === product._id);
-
-    if (productIndex !== -1) {
-        shopCartItems.products[productIndex] = product;
-    } else {
-        shopCartItems.products.push(product);
-    }
-
-    const newCart = _cloneDeep(cart);
-    newCart[shopId] = shopCartItems;
-
-    console.tron.error(newCart);
-
-    return { type: CHANGE_CART_ITEM_QUANTITY, cart: newCart };
-}
-
-export function removeFromCart(shopId, shopName, product, cart) {
-    let shopCartItems = { shopId, shopName, products: [] };
-
-    if (_has(cart, shopId)) {
-        shopCartItems = cart[shopId];
-        _remove(shopCartItems.products, item => item._id === product._id);
-    }
-
-    const newCart = _cloneDeep(cart);
-
-    if (shopCartItems.products.length === 0) {
-        _unset(newCart, shopId);
-    } else {
-        newCart[shopId] = shopCartItems;
-    }
+        try {
+            const response = await AppAxios.get(APIURLs.cart, {
+                headers: {
+                    'Authorization': `bearer ${user.token}`
+                }
+            });
     
-    return { type: REMOVE_ITEM_FROM_CART, cart: newCart };
+            console.tron.warn(response);
+            const cart = {}
+            // _forEach(response.data.cart, )
+        } catch (error) {
+            console.tron.error(error);
+        }
+    };
+}
+
+export function addToCart(shopId, shopName, product, cart, user) {
+    return async dispatch => {
+        const { _id: productId, quantity } = product;
+
+        dispatch(cartItemUpdateStarted(productId));
+
+        let shopCartItems = { shopId, shopName, products: [] };
+
+        const newCart = _cloneDeep(cart);
+    
+        if (_has(newCart, shopId)) {
+            shopCartItems = newCart[shopId];
+        }
+    
+        shopCartItems.products.push(product);
+    
+        newCart[shopId] = shopCartItems;
+
+        if (!_isNil(user)) {
+            try {
+                const updates = [{ productId, quantity }];
+                const response = await updateUserCart(CART_ITEMS_API_ACTIONS.add, updates, user);
+                console.tron.warn(response);
+                dispatch(addCartItemSuccess(productId, newCart));
+            } catch (error) {
+                console.tron.error(error);
+                dispatch(updateUserCartFailed(productId, cart, error));
+            }
+        } else {
+            dispatch(addCartItemSuccess(productId, newCart));
+        }
+    };
+}
+
+export function changeCartProductQuantity(shopId, shopName, product, cart, user) {
+    return async dispatch => {
+        const { _id: productId, quantity } = product;
+
+        dispatch(cartItemUpdateStarted(productId));
+
+        let shopCartItems = { shopId, shopName, products: [] };
+
+        const newCart = _cloneDeep(cart);
+    
+        if (_has(newCart, shopId)) {
+            shopCartItems = newCart[shopId];
+        }
+    
+        const productIndex = _findIndex(shopCartItems.products, item => item._id === productId);
+    
+        if (productIndex !== -1) {
+            shopCartItems.products[productIndex] = product;
+        } else {
+            shopCartItems.products.push(product);
+        }
+    
+        newCart[shopId] = shopCartItems;
+    
+        if (!_isNil(user)) {
+            try {
+                const updates = [{ productId, quantity }];
+                const response = await updateUserCart(CART_ITEMS_API_ACTIONS.change, updates, user);
+                console.tron.warn(response);
+                dispatch(changeCartItemQuantity(productId, newCart));
+            } catch (error) {
+                console.tron.error(error);
+                dispatch(updateUserCartFailed(productId, cart, error));
+            }
+        } else {
+            dispatch(changeCartItemQuantity(productId, newCart));
+        }
+    };
+}
+
+export function removeFromCart(shopId, shopName, product, cart, user) {
+    return async dispatch => {
+        const { _id: productId, quantity } = product;
+
+        dispatch(cartItemUpdateStarted(productId));
+
+        let shopCartItems = { shopId, shopName, products: [] };
+
+        const newCart = _cloneDeep(cart);
+    
+        if (_has(newCart, shopId)) {
+            shopCartItems = newCart[shopId];
+            _remove(shopCartItems.products, item => item._id === productId);
+        }
+    
+        if (shopCartItems.products.length === 0) {
+            _unset(newCart, shopId);
+        } else {
+            newCart[shopId] = shopCartItems;
+        }
+
+        if (!_isNil(user)) {
+            try {
+                const updates = [{ productId, quantity }];
+                const response = await updateUserCart(CART_ITEMS_API_ACTIONS.remove, updates, user);
+                console.tron.warn(response);
+                dispatch(removeCartItemSuccess(productId, newCart));
+            } catch (error) {
+                console.tron.error(error);
+                dispatch(updateUserCartFailed(productId, cart, error));
+            }
+        } else {
+            dispatch(removeCartItemSuccess(productId, newCart));
+        }
+    };
+}
+
+export function uploadUserCart(cart, user) {
+    return async dispatch => {
+        const updates = _flatten(
+            _map(cart, shop => 
+                _map(shop.products, product =>
+                    ({ productId: product._id, quantity: product.quantity })
+                )
+            )
+        );
+        console.tron.error(updates);
+
+        try {
+            const response = await updateUserCart(CART_ITEMS_API_ACTIONS.add, updates, user);
+            console.tron.warn(response);
+        } catch (error) {
+            console.tron.error(error);
+            dispatch({ type: FAILED_TO_UPLOAD_USER_CART_UPON_LOGIN });
+        }
+    };
 }
 
 export function buyShopProducts(user, products, cart) {
@@ -105,6 +214,51 @@ export function getProductQuantityInCart(product, shopId, cart) {
         item._id === product._id && item.category === product.category);
 
     return cartProduct ? cartProduct.quantity : 0;
+}
+
+function updateUserCart(action, updates, user) {
+    const reqBody = {
+        action,
+        updates
+    };
+
+    return AppAxios.patch(APIURLs.cart, reqBody, {
+        headers: {
+            'Authorization': `bearer ${user.token}`
+        }
+    });
+}
+
+function getUserCartStarted() {
+    return { type: GET_USER_CART_STARTED };
+}
+
+function getUserCartSucess(cart) {
+    return { type: GET_USER_CART_SUCCESS, cart };
+}
+
+function getUserCartFailed(error) {
+    return { type: GET_USER_CART_FAILED, error };
+}
+
+function cartItemUpdateStarted(productId) {
+    return { type: CART_ITEMS_UPDATE_STARTED, productId };
+}
+
+function addCartItemSuccess(productId, newCart) {
+    return { type: ADD_ITEM_TO_CART, cart: newCart, productId };
+}
+
+function removeCartItemSuccess(productId, newCart) {
+    return { type: REMOVE_ITEM_FROM_CART, cart: newCart, productId };
+}
+
+function changeCartItemQuantity(productId, newCart) {
+    return { type: CHANGE_CART_ITEM_QUANTITY, cart: newCart, productId };
+}
+
+function updateUserCartFailed(productId, oldCart, error) {
+    return { type: CART_ITEMS_UPDATE_FAILED, productId, cart: oldCart, error };
 }
 
 function buyingProductsStarted() {
--- App/Store/Reducers/Cart.js
+++ App/Store/Reducers/Cart.js
@@ -3,19 +3,48 @@ import { ActionTypes } from './../Actions';
 const INITIAL_STATE = {
     cart: {},
     error: null,
-    isLoading: false
+    isLoading: false,
+    cartItemsIsLoadingObject: {},
+    updateUserCartError: null
 };
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
+        case ActionTypes.CART_ITEMS_UPDATE_STARTED:
+            return {
+                ...state,
+                cartItemsIsLoadingObject: { ...state.cartItemsIsLoadingObject, [action.productId]: true },
+                updateUserCartError: null
+            };
+
         case ActionTypes.ADD_ITEM_TO_CART:
-            return { ...state, cart: action.cart };
+            return {
+                ...state,
+                cart: action.cart,
+                cartItemsIsLoadingObject: { ...state.cartItemsIsLoadingObject, [action.productId]: false }
+            };
 
         case ActionTypes.CHANGE_CART_ITEM_QUANTITY:
-                return { ...state, cart: action.cart };
+                return {
+                    ...state,
+                    cart: action.cart,
+                    cartItemsIsLoadingObject: { ...state.cartItemsIsLoadingObject, [action.productId]: false }
+                };
 
         case ActionTypes.REMOVE_ITEM_FROM_CART:
-            return { ...state, cart: action.cart };
+            return {
+                ...state,
+                cart: action.cart,
+                cartItemsIsLoadingObject: { ...state.cartItemsIsLoadingObject, [action.productId]: false }
+            };
+
+        case ActionTypes.CART_ITEMS_UPDATE_FAILED:
+            return {
+                ...state,
+                cart: action.cart,
+                cartItemsIsLoadingObject: { ...state.cartItemsIsLoadingObject, [action.productId]: false },
+                updateUserCartError: action.error
+            };
 
         case ActionTypes.BUY_SHOP_CART_PRODUCTS_STARTED:
             return { ...state, isLoading: true, error: null };
@@ -25,6 +54,9 @@ export default function (state = INITIAL_STATE, action) {
 
         case ActionTypes.BUY_SHOP_CART_PRODUCTS_FAILED:
             return { ...state, isLoading: false, error: action.error };
+
+        case ActionTypes.FAILED_TO_UPLOAD_USER_CART_UPON_LOGIN:
+            return { ...state, cart: {} };
 
         default:
             return state;
