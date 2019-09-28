e27fb1dbe54c6a679190d543ff99da2022b63c39
Mohamed Gaber <mohamed.elmoazen@vodafone.com>
2019-09-28 04:24:56 +0200

    [feat] Added account screen

--- App/Screens/Account/Account.Styles.js
+++ App/Screens/Account/Account.Styles.js
@@ -1,5 +1,38 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
+import { Colors } from '../../Theme';
 
+export default StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    // backgroundColor: Colors.whiteColorHexCode
+  },
+  activityIndicatorContainerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  userNameContainerStyle: {
+    margin: 10
+  },
+  userNameTextStyle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.brandColorHexCode
+  },
+  accountOptionsContainerStyle: {
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: Colors.whiteColorHexCode
+  },
+  accountOptionButtonStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5
+  },
+  accountOptionButtonTextStyle: {
+    marginLeft: 5,
+    fontSize: 16,
+    color: Colors.blackColorHexCode
+  }
 });
--- App/Screens/Account/Account.js
+++ App/Screens/Account/Account.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  Platform,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from 'react-native';
+import { Actions } from 'react-native-router-flux'
+
+import { Icon, IconTypes } from './../../Components';
+
+import { Colors } from '../../Theme';
 
 import styles from './Account.Styles';
 
-class Account extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, backgroundColor: 'red' }}>
 
+const TouchableComponent = Platform.OS === 'ios' ?
+      TouchableOpacity : TouchableNativeFeedback;
+
+class Account extends Component {
+  onShippingAddressesPress = () => {
+    Actions.addressBook();
+  }
+
+  render() {
+    const { isGettingUserInfo, userInfo = {} } = this.props;
+    const { fullName } = userInfo || {};
+
+    console.tron.error(this.props);
+    return (
+      <View style={styles.containerStyle}>
+        {
+          isGettingUserInfo
+          ? (
+            <View style={styles.activityIndicatorContainerStyle}>
+              <ActivityIndicator color={Colors.brandColorHexCode} size='small' />
             </View>
-        );
-    }
+          )
+          : (
+            <View style={styles.containerStyle}>
+              <View style={styles.userNameContainerStyle}>
+                <Text style={styles.userNameTextStyle}>{`${fullName.firstName} ${fullName.lastName}`}</Text>
+              </View>
+
+              <View style={styles.accountOptionsContainerStyle}>
+                <TouchableComponent onPress={this.onShippingAddressesPress}>
+                  <View style={styles.accountOptionButtonStyle}>
+                    <Icon name='location-pin' type={IconTypes.entypo} color={Colors.brandColorHexCode} />
+                    <Text style={styles.accountOptionButtonTextStyle}>Shipping Addresses</Text>
+                  </View>
+                </TouchableComponent>
+              </View>  
+            </View>
+          )
+        }
+      </View>
+    );
+  }
 }
 
 export default Account;
